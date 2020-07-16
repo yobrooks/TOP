28174991d597760491e1864f32da35c6c17e6283
Yasmine Brooks <yobrooks28@gmail.com>
2020-07-16 19:15:02 -0400

    finished basic functionality; need to make look better and add comments

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,34 +1,40 @@
+/*Global Variables*/
 let myDisplay = document.querySelector(".display");
 const numberButtons = document.querySelectorAll(".number-button");
 const operatorButtons = document.querySelectorAll(".operator-button");
+let calculator = {firstVal:0, secondVal:0, operator:"", index:0,};
 
-const add = (a, b) => a + b;
+/* Basic Math Functions */
+const add = () => calculator.firstVal + calculator.secondVal;
 
-const subtract = (a, b) => a-b;
+const subtract = () => calculator.firstVal - calculator.secondVal;
 
-const multiply = (a, b) => a*b;
+const multiply = () => calculator.firstVal * calculator.secondVal;
 
-const divide = (a, b)=> a/b;
+const divide = ()=> calculator.firstVal / calculator.secondVal;
 
-function operate(operator, numOne, numTwo){
+//function to determine which operation to perform
+function operate(){
     let result = 0;
-    switch(operator){
-        case '+':
-            result = add(numOne, numTwo);
+    switch(calculator.operator){
+        case 'add':
+            result = add();
             break;
-        case '/':
-            result = divide(numOne, numTwo);
+        case 'divide':
+            result = divide();
             break;
-        case '-':
-            result = subtract(numOne, numTwo);
+        case 'subtract':
+            result = subtract();
             break;
-        case '*':
-            result = multiply(numOne, numTwo);
+        case 'multiply':
+            result = multiply();
             break;
         default:
             result = NaN;
             break;
     }
+
+    return result;
 }
 
 //function that updates the display when a number is entered
@@ -36,13 +42,30 @@ function updateDisplay(buttonString, buttonAction){
     if(myDisplay.textContent === "0" && buttonAction === "number"){
         myDisplay.textContent = buttonString;
     }
+    else if(buttonAction !== "number"){
+        calculator.firstVal = Number(myDisplay.textContent);
+        calculator.index = myDisplay.textContent.length + 1;
+        calculator.operator = buttonAction;
+        myDisplay.textContent += buttonString;
+        console.log(calculator.firstVal);
+        console.log(calculator.operator);
+    }
     else{
         myDisplay.textContent += buttonString;
     }
 }
 
 function evaluateExpression(expression){
-    console.log(expression);
+    //find the second number and set it in secondVal of object
+    calculator.secondVal = Number(expression.substring(calculator.index));
+    let result = operate();
+    //get the answer and display it
+    myDisplay.textContent = result.toString();
+    //reset everything, firstVal will be the result
+    calculator.firstVal = result;
+    calculator.secondVal = 0;
+    calculator.index = 0;
+    calculator.operator = "";
 }
 
 //function that runs the calculator buttons
@@ -53,12 +76,17 @@ function evaluateButton(e){
     }
     else if(action === "clear"){
         myDisplay.textContent = "0";
+        calculator.operator = "";
+        calculator.firstVal = 0;
+        calculator.secondVal = 0;
+        calculator.index = 0;
     }
     else{
         updateDisplay(this.textContent, action);
     }
 }
 
+// add event listeners to the buttons
 numberButtons.forEach(button => button.addEventListener('click', evaluateButton));
 operatorButtons.forEach(button => button.addEventListener('click', evaluateButton));
 
